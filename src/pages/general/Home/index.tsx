9a3a804cb9ec0ae06f14c8fcb2a9d84e014393e1
Ademir Guimarães <85326240+demiguic@users.noreply.github.com>
2023-10-17 16:35:28 -0400

    Quadrado da esquerda

--- src/pages/general/Home/index.tsx
+++ src/pages/general/Home/index.tsx
@@ -1,11 +1,11 @@
-import {useAuth} from "@/provider/Auth";
+import { useAuth } from "@/provider/Auth";
 
 export function Home() {
     const style = `
     w-full
     h-full
     flex
-    flex-row
+    flex-column
     border-orange-500
     surface-overlay
     border-3
@@ -19,6 +19,32 @@ export function Home() {
     return (
         <div className={`w-full h-screen flex flex-row m-2 p-4`}>
             <div className={style}>
+                <div className="flex justify-content-between flex-wrap w-full m-4">
+                    <div className="flex align-items-center justify-content-center w-8rem h-8rem bg-primary font-bold border-round m-2">
+                        1
+                    </div>
+                    <div className="flex align-items-center justify-content-center w-8rem h-8rem bg-primary font-bold border-round m-2">
+                        2
+                    </div>
+                </div>
+                
+                <div className="flex justify-content-between flex-wrap w-full m-4">
+                    <div className="flex align-items-center justify-content-center w-8rem h-8rem bg-primary font-bold border-round m-2">
+                        1
+                    </div>
+                    <div className="flex align-items-center justify-content-center w-8rem h-8rem bg-primary font-bold border-round m-2">
+                        2
+                    </div>
+                    <div className="flex align-items-center justify-content-center w-8rem h-8rem bg-primary font-bold border-round m-2">
+                        3
+                    </div>
+                </div>
+
+                <div className="flex justify-content-center flex-wrap w-full m-4">
+                    <div className="flex align-items-center justify-content-center w-8rem h-8rem bg-primary font-bold border-round m-2">
+                        1
+                    </div>
+                </div>
             </div>
             <div className={style}>
             </div>
