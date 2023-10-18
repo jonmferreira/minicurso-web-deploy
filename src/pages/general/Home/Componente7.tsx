0e3f7a9def4e0227d659003ec6d3603027f31a8b
Geovani Lopes Sampaio <77645237+geovanisampaio13@users.noreply.github.com>
2023-10-18 16:42:16 -0400

    lado a e b

--- src/pages/general/Home/Componente7.tsx
+++ src/pages/general/Home/Componente7.tsx
@@ -1,6 +1,23 @@
 import {style} from "@pages/general/Home/index.tsx";
+import { InputTextarea } from 'primereact/inputtextarea';
+import { Button } from 'primereact/button';
+import React, {useState} from 'react';
+import { Slider, SliderChangeEvent } from "primereact/slider";
+import { AutoComplete, AutoCompleteCompleteEvent } from 'primereact/autocomplete';
+import { Knob, KnobChangeEvent } from 'primereact/knob';
 
 export const Componente7 = () => {
+    const [value, setValue] = useState('');
+    const [value1, setValue1] = useState(0);
+    const [value2, setValue2] = useState(0);
+    const [box, setBox] = useState(0);
+    const [knob, setKnob] = useState(0);
+
+    const [items, setItems] = useState<string[]>([]);
+    const search = (event: AutoCompleteCompleteEvent) => {
+        setItems([...Array(10).keys()].map(item => event.query + '-' + item));
+    }
+    
     return <div className={`w-full h-screen flex flex-row m-2 p-4`}>
 
         <div className={style}>
@@ -14,8 +31,13 @@ export const Componente7 = () => {
                 />
             </div>
         </div>
+
         <div className={style}>
-            B
+        <div className="card flex justify-content-center flex-column align-items-center ">
+                <AutoComplete value={box} suggestions={items} completeMethod={search} onChange={(f) => setBox(f.value)} className='m-2'/>
+                <Slider value={value} onChange={(e: SliderChangeEvent) => setValue(e.value)} className="w-14rem m-4" />
+                <Knob value={knob} onChange={(g: KnobChangeEvent) => setKnob(g.value)} />
+            </div>
         </div>
     </div>
 }
